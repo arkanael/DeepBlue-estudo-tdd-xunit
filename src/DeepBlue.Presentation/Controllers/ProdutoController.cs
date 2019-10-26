d197e062664bd991f843adc4985d4766bc8b1fcf
Luiz Guilherme Bandeira <arkanael@gmail.com>
2019-10-26 13:54:56 -0300

    Controler de produtos

---
+++ src/DeepBlue.Presentation/Controllers/ProdutoController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DeepBlue.Presentation.Controllers
+{
+    [Route("api/[controller]")]
+    public class ProdutoController : Controller
+    {
+       [HttpPost]
+       public IActionResult POST()
+        {
+            return Ok();
+        }
+
+        [HttpPut]
+        public IActionResult PUT()
+        {
+            return Ok();
+        }
+
+        [HttpDelete]
+        public IActionResult DELETE()
+        {
+            return Ok();
+        }
+
+        [HttpGet]
+        public IActionResult GET()
+        {
+            return Ok();
+        }
+
+        [HttpGet]
+        public IActionResult GETALL()
+        {
+            return Ok();
+        }
+    }
+}
